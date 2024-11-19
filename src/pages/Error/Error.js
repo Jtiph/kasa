32dd904a903b52a9d5c13ad807b34b60a508257d
Tiphanie Jan <jan.tiphanie@outlook.fr>
2024-11-19 19:51:58 +0100

    refonte des fichiers scss

--- src/pages/Error/Error.js
+++ src/pages/Error/Error.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import NoPage from '../../components/Error/NoPage';
-import './Error.scss'
+import React from "react";
+import { Link } from 'react-router-dom';
+import './Error.scss';
 
-const Error = () => {
-  return (
-    <div>
-      <div className='main-container'>
-        <NoPage />
-      </div>
-    </div>
-  );
-};
+function Error() {
+    return (
+        <div className="error">
+        <h1 className='error__title'>404</h1>
+        <p className="error__subtitle">Oups! La page que vous demandez n'existe pas.</p>
+        <Link to="/" className="error__link">Retourner sur la page d’accueil</Link>
+        </div>
+    )
+}
 
 export default Error;
